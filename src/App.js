b4d8949d13764489d9b0afc1c5eefe1731156859
Sudhanshu Tripathi <54653437+sudhanshu1306@users.noreply.github.com>
2021-03-17 05:57:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-
+import logo from './logo.png';
 
 
 function App(){
   return (
     <div className="overlay">
+    <span className="logo-image"> <img src={logo} /> </span>
     <h1 className="center-text"> Under Maintenance </h1>
     <p className="colour-text"> Meanwhile, you can take a look at the rest of my work <span className="linkBlue"> <a href="http://www.behance.net/mchonjb" target="_blank"> here, at http://behance.net/mchonjb </a> </span></p>
 
