7dd565f5c1bb3fcc01dc32ea5515f7b85797ab87
Sudhanshu Tripathi <54653437+sudhanshu1306@users.noreply.github.com>
2021-03-17 07:11:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import logo from './logo.png';
+import logo from './m_logofixed.png';
 
 
 function App(){
   return (
     <div className="overlay">
-    <span className="logo-image"> <img src={logo} /> </span>
+    <div className="logo-image"> <img src={logo} /> </div>
     <h1 className="center-text"> Under Maintenance </h1>
-    <p className="colour-text"> Meanwhile, you can take a look at the rest of my work <span className="linkBlue"> <a href="http://www.behance.net/mchonjb" target="_blank"> here, at http://behance.net/mchonjb </a> </span></p>
+    <p className="colour-text"> Meanwhile, you can take a look at the rest of my work <span className="linkBlue"> <a href="http://www.behance.net/mchonjb" target="_blank"> here, at behance.net/mchonjb </a> </span></p>
 
     </div>
   )
 }
 export default App;
+
