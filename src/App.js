509103547a69a2ec2d2f3fec0784f94e650d10ec
Sudhanshu Tripathi <54653437+sudhanshu1306@users.noreply.github.com>
2021-03-17 05:47:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App(){
   return (
     <div className="overlay">
     <h1 className="center-text"> Under Maintenance </h1>
-    <p className="colour-text"> Meanwhile, you can take a look at the rest of my work <span className="linkBlue"> <a href="http://www.behance.net/mchonjb" target="_blank"> here, at http://behance.net/mchonjb </a> <span></p>
+    <p className="colour-text"> Meanwhile, you can take a look at the rest of my work <span className="linkBlue"> <a href="http://www.behance.net/mchonjb" target="_blank"> here, at http://behance.net/mchonjb </a> </span></p>
 
     </div>
   )
