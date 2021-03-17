41c5617031f0d28f361e1f98a4632f74ca5a2aee
Sudhanshu Tripathi <54653437+sudhanshu1306@users.noreply.github.com>
2021-03-17 07:29:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import logo from './m_logofixed.png';
 function App(){
   return (
     <div className="overlay">
+    <div className="wrapper">
     <div className="logo-image"> <img src={logo} /> </div>
     <h1 className="center-text"> Under Maintenance </h1>
     <p className="colour-text"> Meanwhile, you can take a look at the rest of my work <span className="linkBlue"> <a href="http://www.behance.net/mchonjb" target="_blank"> here, at behance.net/mchonjb </a> </span></p>
-
+    </div>
     </div>
   )
 }
